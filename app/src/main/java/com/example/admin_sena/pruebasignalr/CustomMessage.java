8c20093893f13026d0198ed9c40f7a5c2e1cde19
Sadainer Hernandez <sadainer@hotmail.com>
2016-04-18 09:59:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/admin_sena/pruebasignalr/CustomMessage.java
@@ -0,0 +1,9 @@
+package com.example.admin_sena.pruebasignalr;
+
+/**
+ * Created by Admin_Sena on 16/04/2016.
+ */
+public class CustomMessage {
+    public String UserName;
+    public String Message;
+}
